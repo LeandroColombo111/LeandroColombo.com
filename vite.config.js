40af7f7bec4cdf1d67055379ebffd3dff3a01c1a
leandrojsd <leandro@jsdagency.com>
2026-01-13 19:20:47 +0000

    Fix: fix viteconfig

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'development' ? '/' : '/LeandroColombo.com/',
+  base: mode === 'development' ? '/' : './',
   plugins: [react()],
 }));
